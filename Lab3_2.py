9d14aaa0b8050d1e0edc4c54989cb845a888b247
shylan14 <shilan2015n@gmail.com>
2023-12-13 14:12:50 +0200

    Create Lab3_2.py

---
+++ Lab3_2.py
@@ -0,0 +1,7 @@
+def square_elements(lst):
+    return [item ** 2 for item in lst]
+
+# Приклад використання
+input_list = [1, 2, 3, 4, 5]
+result = square_elements(input_list)
+print("Список після зведення до квадрату:", result)
